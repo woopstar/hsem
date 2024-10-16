d9d23650dec8ddd6519a0fc0f51587cd53f5de9e
Andreas Krüger <ak@uniscale.com>
2024-10-16 20:10:26 +0200

    fix: cleanup

--- custom_components/hsem/__init__.py
+++ custom_components/hsem/__init__.py
@@ -6,6 +6,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Smoothing Analytics Sensors from a config entry."""
+
     # Forward both sensor and binary_sensor platforms
     await hass.config_entries.async_forward_entry_setups(
         entry, ["sensor", "binary_sensor"]
@@ -15,6 +16,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+
     # Unload both sensor and binary_sensor platforms
     unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     unload_ok = unload_ok and await hass.config_entries.async_forward_entry_unload(
