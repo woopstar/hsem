504dcaae1143dc18c42cbcbb117e74ba60f4df49
Andreas Krüger <ak@uniscale.com>
2024-10-18 11:22:18 +0200

    fix: include ev data to power sensors

--- custom_components/hsem/custom_sensors/house_consumption_power_sensor.py
+++ custom_components/hsem/custom_sensors/house_consumption_power_sensor.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.event import async_track_state_change_event
 
-from ..const import DOMAIN, ICON
+from ..const import DOMAIN, ICON,DEFAULT_HSEM_HOUSE_POWER_INCLUDES_EV_CHARGER_POWER
 from ..entity import HSEMEntity
-from ..utils.misc import get_config_value
+from ..utils.misc import get_config_value, convert_to_float, convert_to_boolean
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,6 +20,8 @@ class HouseConsumptionPowerSensor(SensorEntity, HSEMEntity):
     def __init__(self, config_entry, hour_start, hour_end):
         super().__init__(config_entry)
         self._hsem_house_consumption_power = None
+        self._hsem_ev_charger_power = None
+        self._hsem_house_power_includes_ev_charger_power = None
         self._hour_start = hour_start
         self._hour_end = hour_end
         self._unique_id = (
@@ -32,6 +34,12 @@ class HouseConsumptionPowerSensor(SensorEntity, HSEMEntity):
 
     def set_hsem_house_consumption_power(self, value):
         self._hsem_house_consumption_power = value
+
+    def set_hsem_house_power_includes_ev_charger_power(self, value):
+        self._hsem_house_power_includes_ev_charger_power = value
+
+    def set_hsem_ev_charger_power(self, value):
+        self._hsem_ev_charger_power = value
 
     @property
     def name(self):
@@ -58,6 +66,8 @@ class HouseConsumptionPowerSensor(SensorEntity, HSEMEntity):
         """Return the state attributes."""
         return {
             "house_consumption_power_entity": self._hsem_house_consumption_power,
+            "ev_charger_power_entity": self._hsem_ev_charger_power,
+            "house_power_includes_ev_charger_power_entity": self._hsem_house_power_includes_ev_charger_power,
             "hour_start": self._hour_start,
             "hour_end": self._hour_end,
             "last_updated": self._last_updated,
@@ -68,6 +78,12 @@ class HouseConsumptionPowerSensor(SensorEntity, HSEMEntity):
         """Fetch updated settings from config_entry options."""
         self.set_hsem_house_consumption_power(
             get_config_value(self._config_entry, "hsem_house_consumption_power")
+        )
+        self.set_hsem_ev_charger_power(
+            get_config_value(self._config_entry, "hsem_ev_charger_power")
+        )
+        self.set_hsem_house_power_includes_ev_charger_power(
+            get_config_value(self._config_entry, "hsem_house_power_includes_ev_charger_power")
         )
 
     async def async_added_to_hass(self):
@@ -82,30 +98,56 @@ class HouseConsumptionPowerSensor(SensorEntity, HSEMEntity):
             self._state = 0.0
 
         # Track state changes for the source sensor
-        _LOGGER.info(
-            f"Starting to track state changes for {self._hsem_house_consumption_power}"
-        )
-        async_track_state_change_event(
-            self.hass, [self._hsem_house_consumption_power], self._handle_update
-        )
+        if self._hsem_house_consumption_power:
+            _LOGGER.info(
+                f"Starting to track state changes for {self._hsem_house_consumption_power}"
+            )
+            async_track_state_change_event(
+                self.hass, [self._hsem_house_consumption_power], self._handle_update
+            )
+
+        if self._hsem_ev_charger_power:
+            _LOGGER.info(
+                f"Starting to track state changes for {self._hsem_ev_charger_power}"
+            )
+            async_track_state_change_event(
+                self.hass, [self._hsem_ev_charger_power], self._handle_update
+            )
 
     async def _handle_update(self, event):
         """Handle updates to the source sensor."""
-        current_hour = datetime.now().hour
+        now = datetime.now()
+
+        current_hour = now.hour
 
         if current_hour == self._hour_start:
-            state = self.hass.states.get(self._hsem_house_consumption_power)
+            hsem_house_consumption_power = self.hass.states.get(self._hsem_house_consumption_power)
+            hsem_ev_charger_power = self.hass.states.get(self._hsem_ev_charger_power)
+            hsem_house_power_includes_ev_charger_power = self.hass.states.get(self._hsem_house_power_includes_ev_charger_power)
 
-            if state and state.state:
+            if hsem_house_consumption_power and hsem_house_consumption_power.state:
                 try:
-                    self._state = float(state.state)
+                    hsem_house_consumption_power_state = convert_to_float(hsem_house_consumption_power.state)
+
+                    if hsem_house_power_includes_ev_charger_power:
+                        hsem_house_power_includes_ev_charger_power_state = convert_to_boolean(hsem_house_power_includes_ev_charger_power.state)
+                    else:
+                        hsem_house_power_includes_ev_charger_power_state = DEFAULT_HSEM_HOUSE_POWER_INCLUDES_EV_CHARGER_POWER
+
+                    if hsem_ev_charger_power:
+                        hsem_ev_charger_power_state = convert_to_float(hsem_ev_charger_power.state)
+                    else:
+                        hsem_ev_charger_power_state = 0
+
+                    if hsem_house_power_includes_ev_charger_power_state:
+                        self._state = float(hsem_house_consumption_power_state - hsem_ev_charger_power_state)
+                    else:
+                        self._state = float(hsem_house_consumption_power_state)
                 except ValueError:
-                    _LOGGER.warning(
-                        f"Invalid state value from {self._hsem_house_consumption_power}: {state.state}"
-                    )
                     self._state = 0.0
 
-            self._last_updated = datetime.now().isoformat()
+            self._last_updated = now.isoformat()
+
             _LOGGER.debug(f"Updated state for {self._unique_id}: {self._state}")
             self.async_write_ha_state()
 
