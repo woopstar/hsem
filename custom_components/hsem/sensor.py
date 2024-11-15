1b121e8d624ef49718a01a395a4136421ca6c7fd
Andreas Krüger <ak@uniscale.com>
2024-11-15 09:46:30 +0100

    fix: refactor sensor.py to be more lean

--- custom_components/hsem/sensor.py
+++ custom_components/hsem/sensor.py
@@ -28,75 +28,16 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         None
     """
 
-    # Create the export from the input from hsem_energi_data_service_export
+    # Setup working mode sensor
     working_mode_sensor = WorkingModeSensor(config_entry)
 
-    # # Extract configuration parameters
-    # working_mode_sensor._hsem_huawei_solar_device_id_inverter_1 = get_config_value(
-    #     config_entry, "hsem_huawei_solar_device_id_inverter_1"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_device_id_inverter_2 = get_config_value(
-    #     config_entry, "hsem_huawei_solar_device_id_inverter_2"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_device_id_batteries = get_config_value(
-    #     config_entry, "hsem_huawei_solar_device_id_batteries"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_batteries_working_mode = get_config_value(
-    #     config_entry, "hsem_huawei_solar_batteries_working_mode"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_batteries_state_of_capacity = get_config_value(
-    #     config_entry, "hsem_huawei_solar_batteries_state_of_capacity"
-    # )
-    # working_mode_sensor._hsem_house_consumption_power = get_config_value(
-    #     config_entry, "hsem_house_consumption_power"
-    # )
-    # working_mode_sensor._hsem_solar_production_power = get_config_value(
-    #     config_entry, "hsem_solar_production_power"
-    # )
-    # working_mode_sensor._hsem_solcast_pv_forecast_forecast_today = get_config_value(
-    #     config_entry, "hsem_solcast_pv_forecast_forecast_today"
-    # )
-    # working_mode_sensor._hsem_ev_charger_status = get_config_value(config_entry, "hsem_ev_charger_status")
-    # working_mode_sensor._hsem_ev_charger_power = get_config_value(config_entry, "hsem_ev_charger_power")
-
-    # working_mode_sensor._hsem_battery_max_capacity = get_config_value(
-    #     config_entry, "hsem_battery_max_capacity"
-    # )
-    # working_mode_sensor._hsem_energi_data_service_import = get_config_value(
-    #     config_entry, "hsem_energi_data_service_import"
-    # )
-    # working_mode_sensor._hsem_energi_data_service_export = get_config_value(
-    #     config_entry, "hsem_energi_data_service_export"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_inverter_active_power_control = get_config_value(
-    #     config_entry, "hsem_huawei_solar_inverter_active_power_control"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_batteries_maximum_charging_power = get_config_value(
-    #     config_entry, "hsem_huawei_solar_batteries_maximum_charging_power"
-    # )
-    # working_mode_sensor._hsem_battery_conversion_loss = get_config_value(
-    #     config_entry, "hsem_battery_conversion_loss"
-    # )
-    # working_mode_sensor._hsem_house_power_includes_ev_charger_power = get_config_value(
-    #     config_entry, "hsem_house_power_includes_ev_charger_power"
-    # )
-    # working_mode_sensor._hsem_morning_energy_need = get_config_value(
-    #     config_entry, "hsem_morning_energy_need"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_batteries_grid_charge_cutoff_soc = get_config_value(
-    #     config_entry, "hsem_huawei_solar_batteries_grid_charge_cutoff_soc"
-    # )
-    # working_mode_sensor._hsem_huawei_solar_batteries_tou_charging_and_discharging_periods = get_config_value(
-    #     config_entry, "hsem_huawei_solar_batteries_tou_charging_and_discharging_periods"
-    # )
-
-    # Wait for power, energy and energy average sensors to be set up
-    power_sensors = await async_setup_power_sensors(
-        config_entry,
-        # working_mode_sensor._hsem_house_consumption_power,
-        # working_mode_sensor._hsem_ev_charger_power,
-        # working_mode_sensor._hsem_house_power_includes_ev_charger_power,
-    )
+    # Add power, energy and energy average sensors
+    power_sensors = []
+    for hour in range(24):
+        hour_start = hour
+        hour_end = (hour + 1) % 24
+        sensor = HouseConsumptionPowerSensor(config_entry, hour_start, hour_end)
+        power_sensors.append(sensor)
 
     # Add sensors to Home Assistant
     async_add_entities([working_mode_sensor] + power_sensors)
@@ -125,35 +66,3 @@ async def async_unload_entry(hass, entry):
     if platform:
         return await platform.async_remove_entry(entry)
     return False
-
-
-async def async_setup_power_sensors(
-    config_entry,
-    # hsem_house_consumption_power,
-    # hsem_ev_charger_power,
-    # hsem_house_power_includes_ev_charger_power,
-):
-    """
-    Set up house consumption power sensors for each hour block.
-
-    Args:
-        config_entry: The configuration entry for the sensor setup.
-        hsem_house_consumption_power: The house consumption power data.
-        hsem_ev_charger_power: The EV charger power data.
-        hsem_house_power_includes_ev_charger_power: Boolean indicating if house power includes EV charger power.
-
-    Returns:
-        List of HouseConsumptionPowerSensor objects for each hour block.
-    """
-    sensors = []
-    for hour in range(24):
-        hour_start = hour
-        hour_end = (hour + 1) % 24
-        sensor = HouseConsumptionPowerSensor(config_entry, hour_start, hour_end)
-        # sensor.set_hsem_house_consumption_power(hsem_house_consumption_power)
-        # sensor.set_hsem_ev_charger_power(hsem_ev_charger_power)
-        # sensor.set_hsem_house_power_includes_ev_charger_power(
-        #    hsem_house_power_includes_ev_charger_power
-        # )
-        sensors.append(sensor)
-    return sensors
